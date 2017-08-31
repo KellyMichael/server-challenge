3a57a78c35f994f59daec416296bf871f063dc0c
Michael Kelly <rockymkelly@gmail.com>
2017-08-31 14:59:32 +0000

    added error catching

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,22 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log(req.query.lattlong);
+  if(!req.query.lattlong){
+    response.send('ERROR, missing query \'lattlong\'')
+  }
   const response = res;
   request("http://www.metaweather.com/api/location/search/?lattlong=" + req.query.lattlong, function(err, tres, body){
-    var body = JSON.parse(tres.body);
+    try{
+      var body = JSON.parse(tres.body);
+    }catch(e){
+      res.send('ERROR, invalid query');
+      return
+    }
     var first = body[0].distance;
     var second = body[1].distance;
+    if(!first || !second){
+      response.send('ERROR, invalid query');
+    }
     response.send((second - first) + '');
   });
 });
